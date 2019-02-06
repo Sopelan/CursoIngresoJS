2deac8ec471ae6d07008b8db9283e9eba442b548
alumno <alumno>
2019-02-06 12:11:08 -0300

    aca estan los ejercicios 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	/*
+ var importar;
+ var Descuento;
+importar=importe.value;
+importar=parseInt(importar);
+Descuento= importar-25/100;
+resultado.value=Descuento;
+	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,11 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
+ var Sueldo;
+ var aumento
+Sueldo=sueldo.value;
+Sueldo=parseInt(Sueldo);
+aumento= Sueldo+10/100
+resultado.value=aumento
 	
 }
