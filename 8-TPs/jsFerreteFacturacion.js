a6c76c6c1b51e33e58a97b9ec888f922e9c7d3c4
alumno <alumno@utnfra.com.ar>
2019-02-09 10:58:47 -0300

    aca esta el tp1

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,58 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precio1;
+    var precio2;
+    var precio3;
+    var en1
+    var en2
+    var en3
+     precio1=precioUno.value;
+     precio2=precioDos.value;
+     precio3=precioTres.value;
+     en3=parseInt(precio3);
+     en1=parseInt(precio1);
+     en2=parseInt(precio2);
+var sumar
+sumar=en1 + en2 + en3;
+alert(sumar);
 }
 function Promedio () 
 {
-	
+		var precio1;
+    var precio2;
+    var precio3;
+    var en1
+    var en2
+    var en3
+     precio1=precioUno.value;
+     precio2=precioDos.value;
+     precio3=precioTres.value;
+     en3=parseInt(precio3);
+     en1=parseInt(precio1);
+     en2=parseInt(precio2);
+var promedio
+promedio=(en1 + en2 + en3)/3;
+alert(promedio);
+
 }
 function PrecioFinal () 
 {
-	
+	var precio1;
+    var precio2;
+    var precio3;
+    var en1;
+    var en2;
+    var en3;
+     precio1=precioUno.value;
+     precio2=precioDos.value;
+     precio3=precioTres.value;
+     en3=parseInt(precio3);
+     en1=parseInt(precio1);
+     en2=parseInt(precio2);
+var sumar;
+var preciofinal;
+sumar=en1 + en2 + en3;
+preciofinal=(sumar*0.21)+sumar;
+alert(preciofinal);
 }
