490cb139e50b92f4d1a95cbea4fc487f368a8fe4
alumno <alumno@utnfra.com.ar>
2019-02-09 10:05:57 -0300

    aca estan parciales 1, 2,3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+var precio
+var preciofinal
+
+precio=prompt("precio?" ,"")
+precio=parseInt(precio)
+preciofinal=(precio-(precio/4))
+elPrecioFinal.value=preciofinal
+alert(preciofinal)
+
+
+
 
 }
+
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+ var ancho;
+  var largo;
+  var rectangulo;
+  ancho=prompt("de cuanto es tu ancho", "");
+  ancho=parseInt(ancho);
+  largo=prompt("de cuanto es tu alto", "");
+  largo=parseInt(largo);
+  rectangulo=(ancho*2+largo*2);
+  alert("el perimetro es de " + rectangulo);
 
 }
