f62040d88af2c2bdda7ef71102951086a8c66af6
Sopelan <47353511+Sopelan@users.noreply.github.com>
2019-02-10 17:14:37 -0300

    aca estan los recuperatorios 1,2 ,3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,12 @@
 function mostrar()
 {
-
+ var precio;
+ var descuento;
+ var preciofinal;
+ precio=prompt("precio?", " ");
+ descuento=prompt("descuento?", " ");
+ precio=parseFloat(precio);
+ descuento=parseFloat(descuento);
+ preciofinal=precio-(precio*descuento/precio);
+ elPrecioFinal.value=preciofinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
-
+ var ancho;
+ var alto;
+ ancho=prompt("su ancho?" ,"");
+ ancho=parseFloat(ancho);
+ alto=prompt("su largo?" , "");
+ alto=parseFloat(alto);
+ var perimetro;
+ perimetro=(ancho+alto)*2;
+ alert("su perimetro es de " + perimetro);
 }
