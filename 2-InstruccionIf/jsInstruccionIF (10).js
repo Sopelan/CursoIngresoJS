3629f4cd67928fe34a5099245dd8a45b1e55ed44
Marcos Sopelan <Marcossopelana@gmail.com>
2019-02-13 15:30:56 -0300

    ins 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+var namram
+numran=Math.floor((Math.random()*10)+1)
+numran=parseInt(numran)
+alert(numran)
+if (numran>=9) 
+{alert("EXCELENTE");	
+}
+else if(numran>=4)
+{alert("APROBO");	
+}
+else{alert("vamos la proxima se puede");
+}
 
 }//FIN DE LA FUNCIÓN
