238fe08a504cb0379d09c22a13e77c406eb265f2
Sopelan <47353511+Sopelan@users.noreply.github.com>
2019-02-19 22:25:06 -0300

    parcial 5 y 6

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+//Mercurio, Venus, la Tierra, Marte, Júpiter, Saturno, Urano, Neptuno y Plutón.
+var planeta
+planeta=prompt("su planeta?" , " ");
+switch (planeta) {
+case "tierra":
+alert("aca vivimos.");
+break;
+case "mercurio":
+case "venus":
+case "marte":
+alert("aca hace mas calor.");
+break;
+case "jupiter":
+case "saturno":
+case "urano":
+case "neptuno":
+alert("aca hace mas frio.")
+break;
+default:
+alert("Aclaración: los planetas se ingresan en minúscula");
+break;
 
 }
+}
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
-{
+{var dia
+	dia=prompt("el dia?", " ");
+switch(dia)
+{case "lunes":
+case "martes":
+case "miercoles":
+case "jueves":
+case "viernes":
+alert("a trabajar");
+break;
+case "sabado":
+case "domingo":
+alert("buen finde");
+break;
+default:
+{alert("no es un dia valido");
+
+}
+
+}
+
+
+
+
+
 
 }
