69cd6d31c38c13f3242c7ab4a293ae7f747b1efe
Sopelan <47353511+Sopelan@users.noreply.github.com>
2019-02-27 16:42:18 -0300

    parcial 7 , 8 y 9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,81 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperatura;
+	var temperaturapares=0;
+	var marcamaspesado;
+	var cantidadproductos0grados=0;
+	var sumapeso=0;
+	var promediopeso;
+	var pesomax;
+	var pesomin;
+	var pesomax2;
+	var pesomin2;
+	var contador=0;
+	var respuesta=true
+	while(respuesta==true)
+	{
+		contador=contador+1
+		marca=prompt("cual es la marca del producto?");
+		peso=prompt("cual es el peso del producto?");
+		peso=parseFloat(peso);
+		while(peso>100 || peso<1 || isNaN(peso))
+		{
+			alert("error");
+			alert("ingrese un numero entre 1 y 100");
+			peso=prompt("cual es el peso del producto?");
+			peso=parseFloat(peso);
+		}
+		temperatura=prompt("cual es la temperatura?");
+		temperatura=parseFloat(temperatura);
+		while(temperatura>30 || temperatura<-30 || isNaN(temperatura))
+		{
+			alert("error");
+			alert("ingrese una temperatura entre -30 y 30");
+			temperatura=prompt("cual es la temperatura?");
+			temperatura=parseFloat(temperatura);
+		}
+		if(temperatura % 2 == 0 && temperatura!=0)
+		{
+			temperaturapares=temperaturapares+1;
+		}
+		if (temperatura<0)
+		{
+			cantidadproductos0grados=cantidadproductos0grados+1;
+		}
+		sumapeso=peso+sumapeso;
+		if (contador==1)
+		{	
+			pesomax=peso;
+			pesomin=peso;
+			marcamaspesado=marca
+		}
+		else
+		{
+			if (pesomax<peso) 
+			{
+				pesomax=peso;
+				marcamaspesado=marca;
+			}
+			else
+			{
+				if (pesomin>peso) 
+				{
+					pesomin=peso;
+				}
+			}
+		}
+		respuesta=confirm("queres poner otro producto?")
+	}
+	promediopeso=sumapeso/contador
+	document.open();
+	document.write("<br>La cantidad de temperaturas pares es "+ temperaturapares +  "</br>");
+	document.write("<br>La marca del producto más pesado es "+ marcamaspesado + "</br>");
+	document.write("<br>La cantidad de productos que se conservan a menos de 0 grados es "+ cantidadproductos0grados + "</br>");
+	document.write("<br>El promedio del peso de todos los productos es "+ promediopeso+ "</br>");
+	document.write("<br>El peso máximo es "+ pesomax + "</br>");
+	document.write("<br>El peso mínimo es "+ pesomin + "</br>");	
+	document.close();
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,64 +1,64 @@
 function mostrar()
 {
-var sexo;
-var numero;
-var contador=0;
-var suma=0;
-var minimo;
-var sexo2;
-var cantidadvaronesaprobados=0;
-while(contador<5)
+	var sexo;
+	var numero;
+	var contador=0;
+	var suma=0;
+	var minimo;
+	var sexo2;
+	var cantidadvaronesaprobados=0;
+	while(contador<5)
 	{
-	contador=contador+1;
-	sexo=prompt("sos de sexo f o m?");	
-	while(isNaN(sexo)==false || (sexo!="f" && sexo!="m"))
+		contador=contador+1;
+		sexo=prompt("sos de sexo f o m?");	
+		while(isNaN(sexo)==false || (sexo!="f" && sexo!="m"))
 		{
-		sexo=prompt("error , sos de sexo f o m?");
+			sexo=prompt("error , sos de sexo f o m?");
 		}
-	numero=prompt("eligir un numero entre 0 y 10");
-	numero=parseInt(numero);	
-	while(isNaN(numero)==true || numero<0 || numero>10)
+		numero=prompt("eligir un numero entre 0 y 10");
+		numero=parseInt(numero);	
+		while(isNaN(numero)==true || numero<0 || numero>10)
 		{
-		numero=prompt("error, eligir un numero entre 0 y 10");
-		numero=parseInt(numero);
+			numero=prompt("error, eligir un numero entre 0 y 10");
+			numero=parseInt(numero);
 		}
-	if (sexo=="m" && numero>=6)
+		if (sexo=="m" && numero>=6)
 		{
-		cantidadvaronesaprobados=cantidadvaronesaprobados+1;
+			cantidadvaronesaprobados=cantidadvaronesaprobados+1;
 		}	
-	if (contador==1)
+		if (contador==1)
 		{
-		minimo=numero
-		if(sexo=="f")
-			{
-			sexo="femenino";
-			}
-		else
-			{
-			sexo="masculino";
-			}	
-		sexo2=sexo
-		}
-	else
-		{
-		if (minimo>numero) 
-			{
-			minimo=numero;
+			minimo=numero
 			if(sexo=="f")
-				{
+			{
 				sexo="femenino";
-				}
+			}
 			else
-				{
+			{
 				sexo="masculino";
+			}	
+			sexo2=sexo
+		}
+		else
+		{
+			if (minimo>numero) 
+			{
+				minimo=numero;
+				if(sexo=="f")
+				{
+					sexo="femenino";
 				}
-			sexo2=sexo;
+				else
+				{
+					sexo="masculino";
+				}
+				sexo2=sexo;
 			}
 		}
 		
-	suma=numero+suma
+		suma=numero+suma
 	}	
-alert("El promedio de las notas totales es " + suma/contador)
-alert("La nota mas baja es " + minimo +" y el sexo de esa persona es " + sexo2)
-alert("La cantidad de varones que su nota haya sido mayor o igual a 6 son: "+ cantidadvaronesaprobados)
+	alert("El promedio de las notas totales es " + suma/contador)
+	alert("La nota mas baja es " + minimo +" y el sexo de esa persona es " + sexo2)
+	alert("La cantidad de varones que su nota haya sido mayor o igual a 6 son: "+ cantidadvaronesaprobados)
 }
